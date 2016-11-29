2d1eefa818292429c3f57a41fc5ddfd0d3f9a013
bejita123 <bsj940528@gmail.com>
2016-11-29 19:18:34 +0900

    ㅇㅇ

--- Net_HarliGail/src/ClientStart.java
+++ Net_HarliGail/src/ClientStart.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -100,7 +101,7 @@ public class ClientStart extends JFrame implements ActionListener, Runnable{
     		s=new Socket("localhost", 65535);		// s=>server
     		in=new BufferedReader(new InputStreamReader(s.getInputStream()));		//서버로 값을 읽어들임
 			out=s.getOutputStream();												//서버로 값을 보냄
-			/*out.write((Function.LOGIN+"|"+id+"|"
+			/*out.write((Protocol.LOGIN+"|"+id+"|"
 					+pass+"\n").getBytes());*/
     	}catch(Exception ex){}
     	
@@ -119,15 +120,304 @@ public class ClientStart extends JFrame implements ActionListener, Runnable{
 				StringTokenizer st=new StringTokenizer(msg, "|");//  shift + \
 				int protocol=Integer.parseInt(st.nextToken());
 				switch(protocol){
-				
+				case Protocol.YOURTURN:				//0.자기차례일 때 카드뒤집기 버튼활성화
+				{
+					//gameR.cardOpen.setBorderPainted(false);     	
+				//	gameR.cardOpen.setContentAreaFilled(false);
+					//gameR.cardOpen.setEnabled(true);
 				}
-				
-				
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+				break;
+				case Protocol.DELROW: 		//1.게임종료한 client 정보 접속자 List 에서 삭제
+				{
+					int rowIndex=(Integer.parseInt(st.nextToken()));		//rowIndex=delIndex
+					System.out.println("삭제 줄: "+rowIndex);
+			//		waitR.model2.removeRow(rowIndex);							//접속자리스트에서 삭제
+				}
+				break;
+				case Protocol.CLIENTEXIT:	//2.waitRoom 채팅방에 00님이 나가셨습니다 전송
+				{
+					waitR.chat.append(st.nextToken()+"\n");
+				//	waitR.bar.setValue(waitR.bar.getMaximum());
+				}
+				 break;
+				  case Protocol.MYLOG:				//1.window타이틀에 사용자이름 업데이트
+				  {
+					  String id=st.nextToken();
+					  setTitle(id);
+					  card.show(getContentPane(), "WR");	//waitingroom으로 창 전환
+					  
+				  }
+				  break;
+				  
+				  case Protocol.LOGIN:				//2.접속자테이블에 사용자 업데이트
+				  {
+					  String[] data={
+						st.nextToken(),	 
+						st.nextToken()
+						
+					  };
+					//  waitR.model2.addRow(data);	
+				  }
+				  break;
+				  
+				  case Protocol.ROOMUSER:				//2.게임룸 유저테이블에 유저업데이트
+				  {
+					  System.out.println("In-ROOMUSER");
+					  String[] data={
+						st.nextToken()
+					  };
+					//  gameR.model1.addRow(data);	
+				  }
+				  break;
+				  case Protocol.OUTUSER:
+				  {
+					  int rowIndex=(Integer.parseInt(st.nextToken()));		//rowIndex=delIndex
+					  System.out.println("삭제 줄: "+rowIndex);
+					//  gameR.model1.removeRow(rowIndex);
+				  }
+				  
+				  case Protocol.WAITCHAT1:			//3.채팅할 때(waitroom)
+				  {
+					  waitR.chat.append(st.nextToken()+"\n");
+					//  waitR.bar.setValue(waitR.bar.getMaximum());
+				  }
+				  break;
+				  
+				  case Protocol.ROOMCHAT:			//3.채팅할 때(gameWindow)
+				  {
+					  gameR.chat.append(st.nextToken()+"\n");
+					//  gameR.setValue(gameR.bar.getMaximum());
+					  validate();
+				  }
+				  break;
+				  
+				  case Protocol.NOTOVERLAP:			//4.ID가 중복되지 않을 때
+				  {
+					  JOptionPane.showMessageDialog(this,"ID가 중복되지 않습니다");
+					//  join.ck=true;
+					  join.pwdF.requestFocus();
+				  }
+				  break;
+				  
+				  case Protocol.OVERLAP:			//4.ID가 중복될 때
+				  {
+					  JOptionPane.showMessageDialog(this,
+								"ID가 중복됩니다. 다시 입력하세요.");
+					  //join.ck=false;
+					  join.nameF.requestFocus();
+				  }
+				  break;
+				  
+				  case Protocol.MAKEROOM:			//5.client가 방만들기 확인 버튼을 눌렀을 때(게임창 전환)
+				  {	
+					  String roomId=st.nextToken();		//게임룸 만든 사람 	id
+					  String roomName=st.nextToken();	//새로 만든 게임룸의 	이름
+					  String humanNum=st.nextToken();		//현재인원수	//아직 안쓰임
+					  String capaNum=st.nextToken();		//최대인원수	//아직 안쓰임
+					  setTitle("방장_"+roomId+"    "+"방제_"+roomName);	
+					//  gameR.b5.setEnabled(false); 	//시작버튼 비활성화
+					  gameR.chat.setText("");
+					  card.show(getContentPane(), "GW"); 		//게임창으로 전환
+					  
+				  }
+				  break;
+
+				  case Protocol.ROOMINFORM:			//5.client가 방만들기 확인 버튼을 눌렀을 때(waitRoom의 리스트에 방 추가)
+				  {	
+					  String roomType=st.nextToken();	//공개비공개
+					  String roomName=st.nextToken();	//게임룸의 이름
+					  String nnum=st.nextToken();		//현재인원
+					  String num=st.nextToken();		//최대인원
+					  String pos=st.nextToken();		//방상태(게임대기중)
+					  String[] data={roomType, roomName, nnum, num, pos};	
+					 // waitR.model1.addRow(data);			//waitRoom의 리스트에 방 추가						  
+					  waitR.repaint();
+				  }
+				  break;
+				  
+				  
+				  case Protocol.JOINROOM:			//6.방에 들어가기 했을 때(인원 수에따라 입장 가능 여부)
+				  {
+					  String result=st.nextToken();
+					  if(result.equals("TRUE"))
+					  {
+						  String roomMaker=st.nextToken();
+						  String roomName=st.nextToken();
+						  setTitle("방장_"+roomMaker+"    "+"방제_"+roomName);	
+						//  gameR.b5.setEnabled(false); 	//시작버튼 비활성화
+						  gameR.chatF.setText("");
+						  card.show(getContentPane(), "GW");
+						  //준비버튼 활성화
+						  gameR.b4.setEnabled(true); 
+						  validate();
+					  }
+					  else
+					  {
+						  JOptionPane.showMessageDialog(this,"방이 꽉찼습니다.");
+					  }
+				  }
+				  break;
+				  
+				  case Protocol.ROOMREADY:			//6.준비버튼 눌렀을 때 버튼 비활성화
+				  {
+					  System.out.println("최종적으로 준비전달받음");
+					  gameR.b4.setEnabled(false); 							//준비버튼비활성화
+				  }
+				  break;
+				  
+				  case Protocol.ROOMREADYBUTTON:			//7.모두준비했을 때 방장만 시작 활성화
+				  {
+					  System.out.println("방장의 권한으로 시작버튼 활성화");
+					//  gameR.b5.setEnabled(true); 							//준비버튼비활성화
+					  
+				  }
+				  break;
+//				  case Protocol.GAMESTART:			//7.모두준비했을 때 방장만 시작 활성화
+//				  {
+//					  System.out.println("방장의 권한으로 시작버튼 활성화");
+//					  gw.cardOpen.setBorderPainted(false);      
+//						gw.cardOpen.setContentAreaFilled(false);
+//						gw.cardOpen.setEnabled(false);
+//					  
+//				  }
+//				  break;
+				  /*[방인원변경 ] ->*/
+				  case Protocol.CHGROOMUSER:
+				  {
+					  //대기실 방 List table 의 특정 Row 의 방인원이 변경됨
+					  int row=Integer.parseInt(st.nextToken());
+					  String userNum=st.nextToken();
+				//	  waitR.model1.setValueAt(userNum, row, 2);
+					  waitR.repaint();
+				  }
+				  break;
+
+				  /*[유저상태변경] ->*/
+				  case Protocol.CHGUSERPOS:
+				  {
+					  int row=Integer.parseInt(st.nextToken());	//방번호
+					  System.out.println("\\\\\\--->"+row);
+					  String pos=st.nextToken();		//현재인원수
+					//  waitR.model2.setValueAt(pos, row, 1);
+					  waitR.repaint();
+				  }
+				  break;
+				  
+				  /*[방상태변경 ] ->*/
+				  case Protocol.CHGROOMSTATE:
+				  {
+					  //대기실 방 List table 의 특정 Row 의 방인원이 변경됨
+					  int row=Integer.parseInt(st.nextToken());		//방번호
+					  String roomState=st.nextToken();				//방상태
+					//  waitR.model1.setValueAt(roomState, row, 4);
+					  waitR.repaint();
+				  }
+				  break;
+				  
+				  /*[방나가기] ->*/
+				  case Protocol.DELROOM: //방에 사용자가 없에 방삭제 메시지 받음
+				  {
+					  gameR.chatF.setText("");
+					  int roomRow=Integer.parseInt(st.nextToken());
+					  System.out.println(roomRow+"방 삭제");
+					//  waitR.model1.removeRow(roomRow);
+					  waitR.repaint();
+				  }
+				  break;
+				  case Protocol.REPAINT:
+				  {
+					  String tmpName=st.nextToken();
+					  int b=Integer.parseInt(st.nextToken());
+					  System.out.println("InREPAIT-ID:"+tmpName+"Number:"+b);
+				//	  gameR.UpdateDraw(tmpName, b);						  
+
+				  }
+				  break;
+				  case Protocol.CARDNUM:
+				  {
+					  String tmpName=st.nextToken();			//id
+					  int b=Integer.parseInt(st.nextToken());	//카드수
+					  System.out.println("InCARDNUM-ID:"+tmpName+"Number:"+b);
+				//	  gameR.UpdateCardNum(tmpName, b);
+				  }
+				  break;
+				  case Protocol.DEAD:
+				  {
+					  gameR.chat.append("당신은 죽었습니다.\n");
+					//  gameR.bell.setEnabled(false);
+					//  gameR.cardOpen.setEnabled(false);
+				  }
+				  break;
+				  case Protocol.UPDATEDEAD:
+				  {
+					  String tmpName=st.nextToken();
+					  gameR.chat.append(tmpName+" 님이 죽었습니다.\n");
+					//  gameR.UpdateDead(tmpName);
+					  validate();
+				  }
+				  break;
+				  case Protocol.BELLSUCCESS:
+				  {
+					  String tmpName=st.nextToken();
+					  gameR.chat.append(tmpName+" 님이 종치기 성공했습니다.\n");
+				//	  gameR.bell.setEnabled(true);
+					//  gameR.CardInit();
+				  }
+				  break;
+				  
+				  case Protocol.BELLFAIL:
+	                 {
+	                    String tmpName=st.nextToken();
+	                    gameR.chat.append(tmpName+"님이 종치기 실패하였습니다.\n");
+	                 //   gameR.bell.setEnabled(true);
+	                    validate();
+	                 }
+	              break;
+	             
+				  case Protocol.BELL:
+				  {
+					//  gameR.bell.setEnabled(false);
+				  }
+				  break;
+				  
+				  case Protocol.TURNINFO:
+				  {
+					//  gameR.userName[0]=st.nextToken();
+					//  gameR.userName[1]=st.nextToken();
+					//  gameR.userName[2]=st.nextToken();
+					//  gameR.userName[3]=st.nextToken();
+				  }
+				  break;
+				  case Protocol.EXITFALSE:			//게임시작시 나가기비활성화
+				  {
+				//	  gameR.b6.setEnabled(false);
+				  }
+				  break;
+				  
+				  case Protocol.IDLABEL:			//게임시작시 id라벨 입력
+				  {
+					  String ID=st.nextToken();	//id
+					  for(int i=0; i<4; i++){
+					//	  if(ID.equals(gameR.userName[i])){
+					//		  gameR.laPlayer[i].setText("Player"+(i+1)+": "+ID);
+						  }
+					  }
+				  }
+				  break;/* -> 추후 수정
+				  case Protocol.GAMEEXIT:
+				  {
+					  System.out.println("zzzzz");
+					  String tmpId=st.nextToken();
+					  String tmpMsg=st.nextToken();
+					  gameR.chat.append("게임종료=====>"+tmpId+tmpMsg);
+					  gameR.b4.setEnabled(true);
+					  gameR.b6.setEnabled(true);
+					  gameR.CardInit();
+				  }
+				  break;
+				}*/
+			}catch(Exception ex){validate();}
+			validate();
 		}
 	}
 
